32bd99d40f6ffdd63fb1d72292abfac06505b4d1
yahaylmh <yahyamh01@gmail.com>
2021-08-25 19:18:14 +0430

    Add home list item layout

--- app/src/main/java/com/yaya/apod/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/yaya/apod/ui/fragments/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.yaya.apod.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -21,6 +22,7 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val recyclerView = binding!!.recyclerView
+        binding!!.recyclerView.layoutManager = LinearLayoutManager(activity)
 
     }
 
