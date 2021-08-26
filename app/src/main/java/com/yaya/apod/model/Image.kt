3af37291292144f8f10b64c1d39a07ff3b8e4ff5
yahaylmh <yahyamh01@gmail.com>
2021-08-26 12:46:34 +0430

    Add models

---
+++ app/src/main/java/com/yaya/apod/model/Image.kt
@@ -0,0 +1,12 @@
+package com.yaya.apod.model
+
+class Image {
+    lateinit var copyright: String
+    lateinit var date: String
+    lateinit var explanation: String
+    lateinit var hdurl: String
+    lateinit var media_type: String
+    lateinit var service_version: String
+    lateinit var title: String
+    lateinit var url: String
+}
---
+++ app/src/main/java/com/yaya/apod/model/Video.kt
@@ -0,0 +1,10 @@
+package com.yaya.apod.model
+
+class Video {
+    lateinit var date: String
+    lateinit var explanation: String
+    lateinit var media_type: String
+    lateinit var service_version: String
+    lateinit var title: String
+    lateinit var url: String
+}
