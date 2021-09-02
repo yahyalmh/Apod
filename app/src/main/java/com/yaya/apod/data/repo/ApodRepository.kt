8a4368ad3922117660c8ef64d1c0839d5c74ad51
yahaylmh <yahyamh01@gmail.com>
2021-09-02 14:27:20 +0430

    Change some method name

--- app/src/main/java/com/yaya/apod/data/repo/ApodRepository.kt
+++ app/src/main/java/com/yaya/apod/data/repo/ApodRepository.kt
@@ -3,21 +3,28 @@ package com.yaya.apod.data.repo
 import androidx.lifecycle.LiveData
 import com.yaya.apod.AppExecutors
 import com.yaya.apod.api.ApiResponse
-import com.yaya.apod.api.ApodService
+import com.yaya.apod.api.ApodApi
 import com.yaya.apod.data.db.AppDatabase
 import com.yaya.apod.data.db.dao.ApodDao
 import com.yaya.apod.data.model.Apod
-import com.yaya.apod.util.Util
+import com.yaya.apod.di.NetworkModule
+import com.yaya.apod.util.DateUtil
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class ApodRepository @Inject constructor(
     private val appExecutors: AppExecutors,
-    private val apodService: ApodService,
+    @NetworkModule.ApiWithLiveDataAdapter private val apodApi: ApodApi,
     private val db: AppDatabase,
     private val apodDao: ApodDao
 ) {
+//    fun getApodResultStream(date: String, pageSize: Int): LiveData<PagingData<Apod>> {
+//        return Pager(
+//            config = PagingConfig(enablePlaceholders = false, pageSize = pageSize),
+//            pagingSourceFactory = { ApodPagingSource(this, date) }
+//        ).flow.asLiveData()
+//    }
 
     suspend fun updateApod(apod: Apod) {
         apodDao.update(apod)
@@ -34,24 +41,24 @@ class ApodRepository @Inject constructor(
             }
 
             override fun loadFromDb(): LiveData<Apod> {
-                return apodDao.getByDate(Util.getTodayDate())
+                return apodDao.getByDate(DateUtil.todayDate())
             }
 
             override fun createCall(): LiveData<ApiResponse<Apod>> {
-                return apodService.getTodayContent()
+                return apodApi.getTodayContent()
             }
         }.asLiveData()
     }
 
 
-    fun getContents(endDate: String, count: Int): LiveData<Resource<MutableList<Apod>>> {
+    fun getApods(endDate: String, count: Int): LiveData<Resource<MutableList<Apod>>> {
         return object : NetworkBoundResource<MutableList<Apod>, Array<Apod>>(appExecutors) {
             override fun saveCallResult(item: Array<Apod>) {
-                apodDao.insertAll(*item)
+                apodDao.insertArray(*item)
             }
 
             override fun shouldFetch(data: MutableList<Apod>?): Boolean {
-                return data == null || data.size < count || data[0].date != Util.getTodayDate()
+                return data == null || data.size < count || data[0].date != DateUtil.todayDate()
             }
 
             override fun loadFromDb(): LiveData<MutableList<Apod>> {
@@ -59,8 +66,8 @@ class ApodRepository @Inject constructor(
             }
 
             override fun createCall(): LiveData<ApiResponse<Array<Apod>>> {
-                val startDate = Util.getDateBeforeToday(daysCountBeforeToday = count)
-                return apodService.getContents(startDate = startDate)
+                val startDate = DateUtil.getDateBeforeDate(DateUtil.todayDate(), count)
+                return apodApi.getContents(startDate = startDate)
             }
         }.asLiveData()
     }
