4c1f5bd6149d7b8618d948ad76f5fb44d27bd0b9
yahaylmh <yahyamh01@gmail.com>
2021-09-04 17:11:58 +0430

    Add util method to check internet availibility

--- app/src/main/java/com/yaya/apod/util/AndroidUtils.kt
+++ app/src/main/java/com/yaya/apod/util/AndroidUtils.kt
@@ -1,8 +1,12 @@
 package com.yaya.apod.util
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import javax.inject.Singleton
 import kotlin.math.ceil
+
 
 @Singleton
 class AndroidUtils {
@@ -15,5 +19,35 @@ class AndroidUtils {
                 0
             } else ceil(density * value).toInt()
         }
+
+        @Suppress("DEPRECATION")
+        fun isInternetAvailable(context: Context): Boolean {
+            var result = false
+            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                connectivityManager?.run {
+                    connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)?.run {
+                        result = when {
+                            hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                            hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                            hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                            else -> false
+                        }
+                    }
+                }
+            } else {
+                connectivityManager?.run {
+                    connectivityManager.activeNetworkInfo?.run {
+                        if (type == ConnectivityManager.TYPE_WIFI) {
+                            result = true
+                        } else if (type == ConnectivityManager.TYPE_MOBILE) {
+                            result = true
+                        }
+                    }
+                }
+            }
+            return result
+        }
+
     }
 }
