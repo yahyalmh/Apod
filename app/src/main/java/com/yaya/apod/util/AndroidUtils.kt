7cc9d292e766defb1721d258148ab58ee7b73e9d
yahaylmh <yahyamh01@gmail.com>
2021-09-04 09:52:02 +0430

    Add Android util class to handle utility function in andoird like converting a number to dp

---
+++ app/src/main/java/com/yaya/apod/util/AndroidUtils.kt
@@ -0,0 +1,19 @@
+package com.yaya.apod.util
+
+import android.content.Context
+import javax.inject.Singleton
+import kotlin.math.ceil
+
+@Singleton
+class AndroidUtils {
+
+    companion object {
+        fun dp(context: Context, value: Float): Int {
+            val density = context.resources.displayMetrics.density;
+
+            return if (value == 0f) {
+                0
+            } else ceil(density * value).toInt()
+        }
+    }
+}
