6f12fd3d450a30d9f64210f94551548ec490b3ca
yahaylmh <yahyamh01@gmail.com>
2021-09-04 09:46:31 +0430

    Chanage how to get new data and refresh strategy from server

--- app/src/main/java/com/yaya/apod/data/repo/ApodRemoteMediator.kt
+++ app/src/main/java/com/yaya/apod/data/repo/ApodRemoteMediator.kt
@@ -1,16 +1,18 @@
 package com.yaya.apod.data.repo
 
+import android.content.Context
+import android.widget.Toast
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadType
 import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
 import androidx.room.withTransaction
-import com.airbnb.lottie.utils.Utils
 import com.yaya.apod.api.ApodApi
 import com.yaya.apod.data.db.AppDatabase
 import com.yaya.apod.data.model.Apod
 import com.yaya.apod.di.NetworkModule
 import com.yaya.apod.util.DateUtil
+import dagger.hilt.android.qualifiers.ApplicationContext
 import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
@@ -19,40 +21,64 @@ import javax.inject.Singleton
 @ExperimentalPagingApi
 @Singleton
 class ApodRemoteMediator @Inject constructor(
+    @ApplicationContext private val context: Context,
     @NetworkModule.ApiNormal private val apodApi: ApodApi,
     private val db: AppDatabase
 ) : RemoteMediator<Int, Apod>() {
 
     override suspend fun load(loadType: LoadType, state: PagingState<Int, Apod>): MediatorResult {
         try {
-            val endDate = when (loadType) {
-                LoadType.REFRESH -> DateUtil.todayDate()
+            when (loadType) {
                 LoadType.PREPEND -> return MediatorResult.Success(endOfPaginationReached = true)
+                LoadType.REFRESH -> {
+                    val lastDate = db.withTransaction {
+                        db.apodDao().getLastDate()
+                    }
+                    val count = db.withTransaction {
+                        db.apodDao().getCount()
+                    }
+                    if (lastDate == null || count == 0) {
+                        val endDate = DateUtil.todayDate()
+                        val startDate = DateUtil.getDateBeforeDate(endDate, state.config.pageSize)
+                        val data =
+                            apodApi.getContentByDatePeriod(startDate = startDate, endDate = endDate)
+                        db.withTransaction {
+                            db.apodDao().insertList(data)
+                        }
+                    } else {
+                        val topDate = db.withTransaction {
+                            db.apodDao().getTopDate()
+                        }
+                        if (topDate != DateUtil.todayDate()) {
+                            val todayApod = apodApi.getTodayContent()
+                            db.withTransaction {
+                                db.apodDao().insert(todayApod)
+                            }
+                        } else {
+                            Toast.makeText(context, "Everything in updated :))", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    }
+                }
                 LoadType.APPEND -> {
                     var lastDate = db.withTransaction {
                         db.apodDao().getLastDate()
                     }
-                    if ( lastDate == null){
+                    if (lastDate == null) {
                         lastDate = DateUtil.todayDate()
                     }
-                    if (lastDate != DateUtil.todayDate()){
+                    if (lastDate != DateUtil.todayDate()) {
                         lastDate = DateUtil.getDateBeforeDate(lastDate, 1)
                     }
-                    lastDate
+                    val startDate = DateUtil.getDateBeforeDate(lastDate, state.config.pageSize)
+                    val data =
+                        apodApi.getContentByDatePeriod(startDate = startDate, endDate = lastDate)
+                    db.withTransaction {
+                        db.apodDao().insertList(data)
+                    }
                 }
             }
-
-            val startDate = DateUtil.getDateBeforeDate(endDate, state.config.pageSize)
-            val data = apodApi.getTop(startDate = startDate, endDate = endDate)
-
-            db.withTransaction {
-                if (loadType == LoadType.REFRESH) {
-                    db.apodDao().deleteByDate(startDate=startDate, endDate = endDate)
-                }
-
-                db.apodDao().insertList(data)
-            }
-            return MediatorResult.Success(endOfPaginationReached = data.isEmpty())
+            return MediatorResult.Success(endOfPaginationReached = false)
         } catch (e: IOException) {
             return MediatorResult.Error(e)
         } catch (e: HttpException) {
